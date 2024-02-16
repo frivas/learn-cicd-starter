9ff3e6d0886fa7d8dde2e08655503b6f967af525
Francisco Rivas <franciscojrivash@gmail.com>
2024-02-16 19:12:40 +0100

    Fixed tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,16 +1,24 @@
 package auth
 
 import (
-	"fmt"
+	"net/http"
 	"reflect"
+	// "strings"
 	"testing"
 )
 
 func TestGetAPIKey(t *testing.T) {
-	authHeader := make(map[string][]string)
-	authHeader["Authorization"] = append(authHeader["Authorization"], "abc123")
-	got, _ := GetAPIKey(authHeader)
+	header := http.Header{}
+	header.Set("Authorization", "ApiKey abc123")
+	got, err := GetAPIKey(header)
 	want := "abc123"
+	if err != nil {
+		t.Log(err)
+		// t.Log(header.Get("Authorization"))
+		// h := strings.Split(header.Get("Authorization"), " ")
+		// t.Logf("0 => %q", h[0])
+		// t.Logf("1 => %q", h[1])
+	}
 	if !reflect.DeepEqual(want, got) {
 		t.Errorf("got %q want %q", got, want)
 	}
