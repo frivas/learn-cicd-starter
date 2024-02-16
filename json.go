875616f02bd290556281a1549cde646f7c1e8ae6
Francisco Rivas <franciscojrivash@gmail.com>
2024-02-16 21:00:07 +0100

    Fixing issue with code to satisfy gosec

--- json.go
+++ json.go
@@ -27,5 +27,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	n, err := w.Write(dat)
+	if err == nil {
+		log.Printf("Error writing data : %s", err)
+	}
+	log.Printf("Code => %s", n)
 }
